4a42defacd15544ea5fa0bb35cdcfbb2efc26297
Sebastian Rios <juanriosubeda@gmail.com>
2025-03-24 14:03:10 -0700

    Card re-do

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,8 @@
 import './App.css'
-import Header from './components/Header'
-import MainContent from './components/MainComponent'
-import Footer from './components/Footer'
 
 function App() {
   return (
     <>
-      <div className="div-centered">
-        <div className="div-header">
-          <Header />
-        </div>
-        <div className="div-MainContent">
-          <MainContent />
-        </div>
-        <div className="div-Footer">
-          <Footer />
-        </div>
-      </div>
     </>
   )
 }
