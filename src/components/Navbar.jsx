941226ac307c343f9a372dc2225214621d262e9f
Sebastian Rios <juanriosubeda@gmail.com>
2025-03-24 15:54:13 -0700

    Fun facts about React Finished

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <header>
+      <nav>
+        <img src="src\assets\react.svg" className='react-logo' alt="React logo" />
+        <span>ReactFacts</span>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
