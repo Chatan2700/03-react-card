e5f28c1585f3eeec4d864cb873b571b6f6b83561
Sebastian Rios <juanriosubeda@gmail.com>
2025-03-24 13:47:00 -0700

    Fixed some styling issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,19 @@
 import './App.css'
-
-
-function Header() {
-  return (
-    <header>
-      <img src="src\assets\react.svg" width="40px" alt="React logo" />
-
-    </header>
-  )
-}
-
-function MainContent() {
-  return (
-    <main>
-      <h1>Reason I am excited to learn React</h1>
-      <ol>
-        <li>React is a popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
-        <li>I am more likely to get a job as a front end developer if I know React</li>
-
-
-      </ol>
-    </main>
-  )
-}
-
-function Footer() {
-  return (
-    <footer>
-      <small>© 2024 Ziroll development. All rights reserved.</small>
-    </footer>
-  )
-}
+import Header from './components/Header'
+import MainContent from './components/MainComponent'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <>
-      <div className="centered">
-        <div className='vertical-centered'>
+      <div className="div-centered">
+        <div className="div-header">
           <Header />
+        </div>
+        <div className="div-MainContent">
           <MainContent />
+        </div>
+        <div className="div-Footer">
           <Footer />
         </div>
       </div>
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+    <header className='header-component'>
+      <img src="src\assets\react.svg" className='react-logo' alt="React logo" />
+      <nav>
+        <ul className='nav-list'>
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav >
+    </header >
+  )
+}
